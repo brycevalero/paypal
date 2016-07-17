33c46392b61d824090f0f5b9cec2715e580b968e
Bryce Valero <bryce.valero@gmail.com>
2016-07-17 19:40:23 -0400

    remove creds

--- app/src/main/java/com/brycevalero/www/paypalintegration/PayPalConfig.java
+++ app/src/main/java/com/brycevalero/www/paypalintegration/PayPalConfig.java
@@ -5,6 +5,6 @@ package com.brycevalero.www.paypalintegration;
  */
 public class PayPalConfig {
 
-    public static final String PAYPAL_CLIENT_ID = "ASbH_gg6mMPK-ItKD90VdGj4xles7wLUVTPnf43gveY8zQVkBpQ3wUaIceMVnWX21zJBSUJSSS6eqiN2";
-    public static final String PAYPAL_SECRET = "EBKo28WFCGMSrpad2_dV7ygiisdNYwU0pjYm-sW_iqn1XcOYh9dYcu6aUhA7Zn9taii-erBGEiBIUIe0";
+    public static final String PAYPAL_CLIENT_ID = "";
+    public static final String PAYPAL_SECRET = "";
 }
